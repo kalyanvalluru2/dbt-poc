2c2815e99fabb0fa4c8b9554bfe736b980518743
kalyan.valluru@scotiabank.com <kalyan.valluru@scotiabank.com>
2022-08-24 19:29:17 +0000

    DEVELOPING STAGE MODELS

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,3 +1,17 @@
-select *,
-ordersellingprice - ordercostprice as orderprofit
-from raw.globalmart.orders
+select 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('RAW_ORDERS') }} as o
+left join {{ ref('RAW_CUSTOMER') }} as c
+ON O.CUSTOMERID =  C.CUSTOMERID
+left join {{ ref('RAW_PRODUCT') }} as p
+ON O.PRODUCTID = P.PRODUCTID
